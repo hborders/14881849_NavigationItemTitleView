39ec3bbc5b89d5700a40e56ba6ae53e82c3dccce
Heath Borders <heath.borders@jivesoftware.com>
2013-08-30 14:58:31 -0500

    Initial Commit

---
+++ 14881849_NavigationItemTitleView/HJBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HJBAppDelegate.h
+//  14881849_NavigationItemTitleView
+//
+//  Created by Heath Borders on 8/30/13.
+//  Copyright (c) 2013 Heath Borders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HJBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 14881849_NavigationItemTitleView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  14881849_NavigationItemTitleView
+//
+//  Created by Heath Borders on 8/30/13.
+//  Copyright (c) 2013 Heath Borders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HJBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HJBAppDelegate class]));
+    }
+}
